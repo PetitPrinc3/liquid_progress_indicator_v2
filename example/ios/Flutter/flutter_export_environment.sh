292166cc217135240f8efb98ce6c56636c7f9723
PetitPrinc3 <arthur.reppelin@gmail.com>
2025-07-05 18:34:53 -0400

    few updates

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/guyluz/Programs/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/guyluz/Documents/git/liquid_progress_indicator_v2/example"
+export "FLUTTER_ROOT=C:\Users\arthu\dev\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\arthu\Desktop\GitHub\DefinitelyNotDeezer\liquid_progress_indicator_v2\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
